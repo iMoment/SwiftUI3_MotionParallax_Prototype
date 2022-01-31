8738fb8051562d7c5ee226b42782af8bd465f41e
Stanley Pan <imoment.developer@gmail.com>
2022-01-31 14:51:04 -0500

    Initial Commit

---
+++ MotionParallax/MotionParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionParallaxApp.swift
+//  MotionParallax
+//
+//  Created by Stanley Pan on 2022/01/31.
+//
+
+import SwiftUI
+
+@main
+struct MotionParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
